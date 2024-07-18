8b6efefe04401d2e640b06d78c956a2e16b9616a
PinchenLin <s10325347@gmail.com>
2024-07-18 21:44:02 +0800

    Init

---
+++ HashSet_TestPJ/Assets/Script/ComboDataTEst.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ComboDataTEst : MonoBehaviour
+{
+    private List<int>[] OriginData;
+    private HashSet<int>[] WinIndex;
+    private List<int> supplementIconData;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // OriginData = new List<int>[]
+        // {
+        //     new List<int>{0,1,2},
+        //     new List<int>{3,4,5},
+        //     new List<int>{6,7,8},
+        //     new List<int>{9,10,11},
+        // };
+
+        // WinIndex = new HashSet<int>[]
+        // {
+        //     new HashSet<int>{0,1},
+        //     new HashSet<int>{1},
+        //     new HashSet<int>{2},
+        //     new HashSet<int>{0},
+        // };
+
+        // supplementIconData = new List<int> { 20, 20, 20, 20, 20 };
+
+
+        //將資料補到最後
+
+        // 透過 List 刪資料往前補的特性來做 , !! 由於會往前補 所以要記錄 已刪除次數 , 將索引值的內容 - 已刪除次數
+        // 才會取得正確的 索引值
+
+        // 初始数据
+        List<int>[] OriginData = {
+            new List<int> { 0, 1, 2 },
+            new List<int> { 3, 4, 5 }
+        };
+
+        HashSet<int>[] WinIndex = {
+            new HashSet<int> { 1 },
+            new HashSet<int> { 0, 2 }
+        };
+
+        List<int> supplementIconData = new List<int> { 100, 101, 102, 103, 104, 105 };
+        int ChangeCount = 0;
+
+        // 遍历 WinIndex
+        for (int i = 0; i < WinIndex.Length; i++)
+        {
+            // var indices = WinIndex[i].OrderByDescending(x => x).ToList();
+            List<int> changeData = new List<int>();
+            var tamp = 0;
+            foreach (int index in WinIndex[i])
+            {
+
+                int newValue = supplementIconData[ChangeCount];
+
+                if (tamp != 0)
+                {
+                    // 将原值移到当前数组的尾部
+                    OriginData[i].RemoveAt(Mathf.Abs(tamp - index));
+                    changeData.Add(newValue);
+                }
+                else
+                {
+                    OriginData[i].RemoveAt(index);
+                    changeData.Add(newValue);
+                }
+
+                tamp++;
+                ChangeCount++;
+            }
+
+            OriginData[i].AddRange(changeData);
+        }
+        // 打印结果
+        for (int i = 0; i < OriginData.Length; i++)
+        {
+            string text = $"OriginData[{i}] : ";
+            foreach (var item in OriginData[i])
+            {
+                text += $"{item} ,";
+            }
+            Debug.Log(text);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ HashSet_TestPJ/Assets/Script/HashSetPJ.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Schema;
+using UnityEngine;
+
+public class HashSetPJ : MonoBehaviour
+{
+
+    private HashSet<int>[] TestDataA;
+    private HashSet<int>[] TestDataB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Data = new HashSet<int>[3];
+        TestDataA = new HashSet<int>[]
+        {
+            new HashSet<int>{1,2,3},
+            new HashSet<int>{4,5,6},
+            new HashSet<int>{7,8,9}
+        };
+        TestDataB = new HashSet<int>[]
+        {
+            new HashSet<int>{1,2,3},
+            new HashSet<int>{10,11,12},
+            new HashSet<int>{13,14,15}
+        };
+
+        //==========================================================================================================
+
+        //將兩個HashSet集合的內容 合併 , 重複的不會添加 只添加不重複的內容
+        for (var item = 0; item < TestDataA.Length; item++)
+        {
+            // TestDataA.Union(TestDataB);
+            TestDataA[item].UnionWith(TestDataB[item]);
+        }
+
+        //印值
+        for (var item = 0; item < TestDataA.Length; item++)
+        {
+            string s = "";
+            foreach (var index in TestDataA[item])
+            {
+                s += $"{index},";
+            }
+
+            Debug.Log(s);
+        }
+
+        //============================================================================================
+
+        // 多個HashSet 資料合併 
+        // !! 當作基底的資料 必須有內容
+
+        HashSet<int>[] TestDataC = new HashSet<int>[]
+               {
+            new HashSet<int>{1,3},
+            new HashSet<int>{4,6},
+            new HashSet<int>{3}
+               };
+        HashSet<int>[] TestDataD = new HashSet<int>[]
+        {
+            new HashSet<int>{1,},
+            new HashSet<int>{4,3},
+            new HashSet<int>{1,2}
+        };
+
+        HashSet<int>[] TestDataE = new HashSet<int>[]
+        {
+            new HashSet<int>{0,4},
+            new HashSet<int>{5,0},
+            new HashSet<int>{1}
+        };
+
+
+        List<HashSet<int>[]> totalData = new List<HashSet<int>[]>();
+
+        totalData.Add(TestDataC);
+        totalData.Add(TestDataD);
+        totalData.Add(TestDataE);
+
+        //要回傳的 合併完成的資料
+        // HashSet<int>[] resultData = new HashSet<int>[3];
+        HashSet<int>[] resultData = new HashSet<int>[]
+        {
+            new HashSet<int>{0,},
+            new HashSet<int>{1},
+            new HashSet<int>{2}
+        };
+
+        for (int i = 0; i < totalData.Count; i++)
+        {
+            for (int j = 0; j < resultData.Length; j++)
+            {
+                resultData[j].UnionWith(totalData[i][j]);
+            }
+        }
+
+        //印值測試
+        for (var item = 0; item < resultData.Length; item++)
+        {
+            string s2 = "";
+            foreach (var index in resultData[item])
+            {
+                s2 += $"{index},";
+            }
+
+            Debug.Log(s2);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
